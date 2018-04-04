50d412a9f31b009dfd5079d29fddad0c252797c6
Kate Revitte <k.revitte@gmail.com>
2018-04-04 19:27:12 +0000

    Initial commit

---
+++ models/todo.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var todoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'Name cannot be blank'
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
+
+//name string
+//completed boolean
+//created date
